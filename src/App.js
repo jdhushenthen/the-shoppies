c4efb9f156264be31588999ebcca99a773f19742
Janahan Dhushenthen <jdhushenthen@rogers.com>
2021-01-14 17:05:14 -0500

    Updated README and added comments

--- src/App.js
+++ src/App.js
@@ -3,60 +3,76 @@ import './App.css';
 
 class App extends Component {
 
+  // Constructor to initialize state variables
   constructor(props) {
     super(props)
+    
     this.state = {
-      query: '',
-      results: [],
-      nominations: []
+      query: '',        // Search value entered by user
+      results: [],      // Movie results returned by API
+      nominations: []   // Movie nominations selected by user
     }
 
+    // Check user's session storage for any nominations
     let storedNominations = JSON.parse(sessionStorage.getItem('storedNominations'))
     if (storedNominations != null && storedNominations.length > 0) {
       this.state.nominations = storedNominations
     }
   }
 
+  // Handles button press for nomination removal
   handleRemoveButtonPress = (event) => {
     event.preventDefault()  
     
+    // Remove the movie from the nomination list
     let updatedNominations = []
     updatedNominations.push.apply(updatedNominations, this.state.nominations)
     updatedNominations.splice(updatedNominations.indexOf(event.target.value), 1)
 
+    // Update the nominations state variable
     this.setState({
       query: this.state.query,
       results: this.state.results,
       nominations: updatedNominations
     })
 
+    // Update the nominations list in the session storage
     sessionStorage.setItem('storedNominations', JSON.stringify(updatedNominations))
   }
 
+  // Handles button press for new nomination
   handleNominateButtonPress = (event) => {
     event.preventDefault()  
 
+    // Add the movie to the nomination list
     let updatedNominations = []
     updatedNominations.push.apply(updatedNominations, this.state.nominations)
     updatedNominations.push(event.target.value)
 
+    // Update the nominations state variable
     this.setState({
       query: this.state.query,
       results: this.state.results,
       nominations: updatedNominations
     })
 
+    // Update the nominations list in the session storage
     sessionStorage.setItem('storedNominations', JSON.stringify(updatedNominations))
   }
 
+  // Handles changes in the search field
   handleInputChange = (event) => {
     event.preventDefault()
 
-    const api = 'http://www.omdbapi.com/?apikey=b2798bd0&s=' + event.target.value
+    // API URL for searching movies on OMDB
+    const api = 'https://www.omdbapi.com/?apikey=b2798bd0&s=' + event.target.value
 
+    // Make API call (fetch request)
     fetch(api)
       .then((response) => response.json())
       .then((data) => {
+
+        // Get a list of the movie results
         let movies = []
         if (data.Response === "True") {
           let i;
@@ -67,6 +83,8 @@ class App extends Component {
         return movies
       })
       .then((movies) => {
+
+        // Update the query and results state variables
         this.setState({
           query: event.target.value,
           results: movies,
@@ -75,13 +93,17 @@ class App extends Component {
       })
   }
 
+  // Returns JSX to render
   render() {
     return (
       <div className="App">
-        <div class="Alert" style={{display: this.state.nominations.length == 5 ? 'block' : 'none' }}>
+
+        {/* Banner notification that appears when 5 nominations have been selected */}
+        <div class="Alert" style={{display: this.state.nominations.length === 5 ? 'block' : 'none' }}>
           <strong>Success!</strong> You have selected 5 nominations.
         </div>
 
+        {/* Header that contains logo and title */}
         <div className="Header">
           <h3>
             <img class='Logo' src='the-shoppies-logo.png'></img>
@@ -90,6 +112,8 @@ class App extends Component {
         </div>
 
         <div className="Row">
+
+          {/* Search bar to look up movies on OMDB */}
           <div className="Card">
             <label>Movie title</label>
             <input className="Search" type="text" placeholder="Search OMDB..." onChange={this.handleInputChange}></input>
@@ -97,6 +121,8 @@ class App extends Component {
         </div>
 
         <div className="Row">
+
+          {/* Displays list of search results */}
           <div className="Card">
             <label><b>Results for "{this.state.query}"</b></label>
             <ul>
@@ -104,9 +130,7 @@ class App extends Component {
                 this.state.results.map((item) => {
                   return(
                     <li key={item}>
-                      <span>
-                        {item}
-                      </span>
+                      <span>{item}</span>
                       <span>
                         <button className="Button" type="button" value={item} onClick={this.handleNominateButtonPress} disabled={this.state.nominations.includes(item) ? true : false}>
                           Nominate
@@ -118,6 +142,8 @@ class App extends Component {
               }
             </ul>
           </div>
+
+          {/* Displays list of nominations */}
           <div className="Card">
             <label><b>Nominations</b></label>
             <ul>
@@ -126,7 +152,11 @@ class App extends Component {
                   return(
                     <li key={item}>
                       <span>{item}</span>
-                      <span><button className="Button" type="button" value={item} onClick={this.handleRemoveButtonPress}>Remove</button></span>
+                      <span>
+                        <button className="Button" type="button" value={item} onClick={this.handleRemoveButtonPress}>
+                          Remove
+                        </button>
+                      </span>
                     </li>
                   )
                 })
